fc3d793ef57887a523a822993ca4d9198c0aaa95
skanda umesh <skandaumesh82@gmail.com>
2025-12-25 23:34:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    reactStrictMode: true,
-  }
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
   
-  module.exports = nextConfig
-  
+  // ADD THIS SECTION:
+  headers: async () => {
+    return [
+      {
+        source: '/:path*.html',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/html; charset=utf-8',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
